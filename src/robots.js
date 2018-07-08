6b228b9638847ab7f3246f4bfc1d5e89957f0ad3
Brandon Wind <brandonrwind@gmail.com>
2018-07-08 12:28:06 -0500

    Finished working app

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'Sincer@april.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Heyooo',
+        email: 'Shanna@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Liana Perez',
+        username: 'Lili',
+        email: 'lili@lilisplace.com'
+    },
+    {
+        id: 4,
+        name: 'John Snow',
+        username: 'king',
+        email: 'jon@thewall.com'
+    },
+    {
+        id: 5,
+        name: 'Katniss Everdeen',
+        username: 'Kat',
+        email: 'please@dontchooseme.com'
+    },
+    {
+        id: 6,
+        name: 'John Mayer',
+        username: 'JM',
+        email: 'jm@johnmayer.com'
+    },
+    {
+        id: 7,
+        name: 'Clemetine Bauch',
+        username: 'Samantha',
+        email: 'clementine@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Pat Lebsack',
+        username: 'toodles',
+        email: 'toodles@yahoo.com'
+    },
+    {
+        id: 9,
+        name: 'Chelsea Dietrich',
+        username: 'Chels',
+        email: 'chels@gmail.com'
+    },
+]
