b25d82cdcb581ab951e6445f670b875556f56990
Brandon Wind <brandonrwind@gmail.com>
2018-07-08 21:46:28 -0500

    Added scroll component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 
 class App extends Component {
     constructor() {
@@ -29,7 +30,9 @@ class App extends Component {
             <div className='tc'>
                 <h1>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
-                <CardList robots={filteredrobots} />
+                <Scroll>
+                    <CardList robots={filteredrobots} />
+                </Scroll>
             </div>
         );
     }
